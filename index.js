4280d4dcc20b5b57391b3fa4d8771bc1648c0170
Robert Juliano <rovjuvano@gmail.com>
2014-02-20 08:51:51 -0500

    feat: expands single pair of braces

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var Rx = require('rx');
 module.exports = function(string) {
-	return Rx.Observable.fromArray([string]);
+	var m;
+	if (m = /^(.*)\{(.*)\}(.*)$/.exec(string)) {
+		var prefix = m[1];
+		var parts = m[2].split(',');
+		var suffix = m[3];
+		var obs = new Rx.Subject();
+		Rx.Scheduler.timeout.schedule(function() {
+			parts.forEach(function(part) {
+				obs.onNext(prefix + part + suffix);
+			});
+			obs.onCompleted();
+		});
+		return obs;
+	}
+	return Rx.Observable.return(string);
 };
--- test/mainSpec.js
+++ test/mainSpec.js
@@ -16,4 +16,20 @@ describe('rxjs-brace-expand', function() {
 			);
 		})
 	})
+
+	context('with single pair of braces', function() {
+		it('combines parts', function(done) {
+			var pattern = 'A{1,2}Z';
+			var result = [];
+			braceExpand(pattern).subscribe(
+				function(string) { result.push(string) },
+				function(err) { done(err) },
+				function() {
+					var expect = ['A1Z', 'A2Z'];
+					result.should.eql(expect);
+					done();
+				}
+			);
+		})
+	})
 })
