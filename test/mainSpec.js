a53c855038ca5d84ff198a27ccac2edc84fa69e2
Robert Juliano <rovjuvano@gmail.com>
2014-02-20 20:05:52 -0500

    refactor tests

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -1,51 +1,33 @@
 require('should');
 var braceExpand = require('..');
 var context = describe;
+var _it = it;
+it = function(message, pattern, expected) {
+	if (!expected) {
+		expected = [pattern];
+	}
+	_it(message, function(done) {
+		var result = [];
+		braceExpand(pattern).subscribe(
+			function(string) { result.push(string) },
+			function(err) { done(err) },
+			function() {
+				result.should.eql(expected);
+				done();
+			}
+		);
+	});
+};
 describe('rxjs-brace-expand', function() {
 	context('with no braces', function() {
-		it('expands to given string', function(done) {
-			var pattern = ''+Math.random();
-			var result = [];
-			braceExpand(pattern).subscribe(
-				function(string) { result.push(string) },
-				function(err) { done(err) },
-				function() {
-					result.should.eql([pattern]);
-					done();
-				}
-			);
-		})
+		it('expands to given string', ''+Math.random());
 	})
 
-	context('with a single pair of braces', function() {
-		it('combines parts', function(done) {
-			var pattern = 'A{1,2}Z';
-			var result = [];
-			braceExpand(pattern).subscribe(
-				function(string) { result.push(string) },
-				function(err) { done(err) },
-				function() {
-					var expect = ['A1Z', 'A2Z'];
-					result.should.eql(expect);
-					done();
-				}
-			);
-		})
-	})
+	describe('{,}', function() {
+		it('expands for each string in list', 'A{1,2,3}Z', ['A1Z', 'A2Z', 'A3Z']);
+	});
 
-	context('with a single sequence ascending', function() {
-		it('combines parts', function(done) {
-			var pattern = 'A{1..3}Z';
-			var result = [];
-			braceExpand(pattern).subscribe(
-				function(string) { result.push(string) },
-				function(err) { done(err) },
-				function() {
-					var expect = ['A1Z', 'A2Z', 'A3Z'];
-					result.should.eql(expect);
-					done();
-				}
-			);
-		})
-	})
+	describe('{..}', function() {
+		it('expands for each integer in range', 'A{1..3}Z', ['A1Z', 'A2Z', 'A3Z']);
+	});
 })
