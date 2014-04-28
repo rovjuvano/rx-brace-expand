e0f3d18ec37f77bbe1f0fdf3bfe3d1629b8723db
Robert Juliano <rovjuvano@gmail.com>
2014-04-27 22:39:48 -0400

    refactor: break into smaller functions

--- index.js
+++ index.js
@@ -7,31 +7,36 @@ module.exports = function(string) {
 		var suffix = m[3];
 		var obs = new Rx.Subject();
 		Rx.Scheduler.timeout.schedule(function() {
-			if (parts.length === 1 && (m = /^(-?\d+)\.\.(-?\d+)(?:\.\.-?(\d+))?$/.exec(parts[0]))) {
-				var i = +m[1];
-				var j = +m[2];
-				var step = +m[3] || 1;
-				if (i < j) {
-					while (i<=j) {
-						obs.onNext(prefix + i + suffix);
-						i += step;
-					}
-				}
-				else  {
-					while (i>=j) {
-						obs.onNext(prefix + i + suffix);
-						i -= step;
-					}
-				}
-			}
-			else {
-				parts.forEach(function(part) {
-					obs.onNext(prefix + part + suffix);
-				});
-			};
-			obs.onCompleted();
+			expand(obs, parts, prefix, suffix);
 		});
 		return obs;
 	}
 	return Rx.Observable.return(string);
 };
+function expand(obs, parts, prefix, suffix) {
+	var m;
+	if (parts.length === 1 && (m = /^(-?\d+)\.\.(-?\d+)(?:\.\.-?(\d+))?$/.exec(parts[0]))) {
+		expand_range(obs, +m[1], +m[2], +m[3] || 1, prefix, suffix);
+	}
+	else {
+		expand_list(obs, parts, prefix, suffix);
+	};
+	obs.onCompleted();
+}
+function expand_range(obs, i, j, step, prefix, suffix) {
+	var count = Math.floor( (j - i) / step );
+	if (count < 0) {
+		count = -count;
+		step = -step;
+	}
+	count++;
+	while (count--) {
+		obs.onNext(prefix + i + suffix);
+		i += step;
+	}
+}
+function expand_list(obs, parts, prefix, suffix) {
+	parts.forEach(function(part) {
+		obs.onNext(prefix + part + suffix);
+	});
+}
